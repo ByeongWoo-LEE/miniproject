2e3f4969db7c4fb72be8a21f0fda53125d2d241c
moon <bwdnr95@gmail.com>
2021-09-15 19:32:45 +0900

    Champions, Champions Rotuer 페이지 추가

---
+++ miniproject/src/components/ChampionsRouterComponent.js
@@ -0,0 +1,15 @@
+import React,{Component} from "react";
+import { Route } from "react-router";
+
+export default function ChampionsRouterComponent (){
+
+    return(
+       <>
+        {/* <Route exact path='/' component={원천지재}/>
+        <Route path='/detail/:{championName}' component={문병욱}/>
+        
+         */}
+        </> 
+    )
+
+}
