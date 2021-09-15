9a8b126394c52a1aefdf8640adf2f515f07c86dd
이병우 <qoddn987@naver.com>
2021-09-15 18:02:47 +0900

    '기본 ui구성'

--- miniproject/src/App.tsx
+++ miniproject/src/App.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import './css/App.css';
+import Nav from './components/Nav';
+import Champions from './components/Champions'
+import Main from './components/Main'
+
+import { Route, Switch } from 'react-router-dom';
+
 function App() {
   return (
     <div className="App">
-        <p>되니?</p>
+       <Nav />
+        <Switch>
+          <Route exact path='/' component={Main}/>
+          <Route path='/champions' component={Champions}/>
+          <Route component={() => <h2>Not Found!</h2>}/>
+        </Switch>
     </div>
   );
 }
