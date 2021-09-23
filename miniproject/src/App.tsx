16dd4f63104b5707f45e96616656bfe349f5a2cb
Myung-Soo <hetnime@gmail.com>
2021-09-23 16:19:27 +0900

    아몰라

--- miniproject/src/App.tsx
+++ miniproject/src/App.tsx
@@ -1,22 +1,27 @@
-import React from 'react';
+import { useState } from 'react';
 import './css/App.css';
-import Nav from './components/Nav';
 import Champions from './components/Champions'
 import Main from './components/Main'
 
-import Header from "./paging/Header.js";
+import Header from "./components/Header.js";
+import SearchbarContainer from "./components/SearchbarContainer.js";
 
-import { Route, Switch } from 'react-router-dom';
-
+import { Button } from '@mui/material';
 
 function App() {
+  const [ show, setShow] = useState(false);
+
   return (
     <div className="App">
-       <Header onDrawerToggle={function (): void {
+      <Header onDrawerToggle={function (): void {
         throw new Error('Function not implemented.');
       } }/>
+      <Main />
+      <SearchbarContainer />
+      <Button onClick={() => setShow(!show)}>검색</Button>
+      { show? <Champions/> : null}
+      {console.log(show)}
     </div>
-    
   );
 }
 export default App;
